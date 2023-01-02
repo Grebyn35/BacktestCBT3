660942ab3afc109b753310b0a1c55a60114865c1
Grebyn35 <elliot.heathnyberg@gmail.com>
2023-01-02 23:49:47 +0100

    init

--- src/main/java/com/example/backtestcbt3/controller/UserController.java
+++ src/main/java/com/example/backtestcbt3/controller/UserController.java
@@ -49,8 +49,8 @@ public class UserController {
     private static CandlestickRepository staticCandlestickRepository;
     private static UserRepository staticUserRepository;
     private static FakeOrderRepository staticFakeOrderRepository;
-    private static double startValue = 4531;
-    private static int leverage = 50;
+    private static double startValue = 1000;
+    private static int leverage = 20;
 
     @GetMapping("/load-data")
     public String loadData(){
